ea5add0c29ccc66941f896f519ecba3ec1eaac51
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-12 01:23:05 +0800

    removed unused code in layout

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 import "../globals.css";
- import { SpaceGrotesk } from "../components/fontsconfigure";
+// import { SpaceGrotesk } from "../components/fontsconfigure";
 
 // import localFont from "next/font/local";
 
