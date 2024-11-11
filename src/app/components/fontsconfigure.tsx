fcbcbf6a1f30d9a582076f780b9a53932665e860
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-12 00:15:53 +0800

    configure fonts config file

---
+++ src/app/components/fontsconfigure.tsx
@@ -0,0 +1,17 @@
+import localFont from "next/font/local";
+export const SpaceGrotesk = localFont({
+    src:[
+      {
+        path:'../fonts/SpaceGrotesk-Medium.ttf',
+        weight:'500',
+        style:'normal',
+      },
+      {
+        path:'../fonts/SpaceGrotesk-Bold.ttf',
+        weight:'700',
+        style:'normal',
+      },
+    ],
+   
+     })
+  
