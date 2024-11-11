aa096777278e0d08bfa525c2ad95b93ef97efb52
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-12 01:06:48 +0800

    created folder for the apis

---
+++ src/pages/api/contact.tsx
@@ -0,0 +1,19 @@
+// pages/api/hello.ts
+
+import { validateYupSchema } from 'formik';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+ 
+  if(req.method === "POST"){
+    const {name, email, message} = req.body;
+    if( !name || !email || !message){
+      return res.status(400).json({ message: 'Bad Request' });
+    }
+  }
+  console.log(req.body);
+
+ return res.status(400).json({ message: 'Bad Request' });
+}
+
+export  default handler;
